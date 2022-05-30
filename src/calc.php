44b93a5280c174e2ba75c3036b09840f6d8dec78
csengekulker <k71608883429@ktch.hu>
2022-05-30 08:58:19 +0200

    Calculation

--- src/calc.php
+++ src/calc.php
@@ -3,8 +3,35 @@
 
 echo file_get_contents('templates/head.html');
 echo file_get_contents('templates/nav.html');
-echo file_get_contents('templates/calc.html');
-echo file_get_contents('templates/foot.html');
+$page = file_get_contents('templates/calc.html');
 
+function calcVolume ($a, $b, $c) {
+
+    $volume = (4 / 3) * pi()*$a*$b*$c;
+    
+    return $volume;
+}
+
+
+if (
+    !empty($_GET['a']) and
+    !empty($_GET['b']) and
+    !empty($_GET['c'])
+) {
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    $c = $_GET['c'];
+
+    $volume = calcVolume($a, $b, $c);
+
+    $page = str_replace('{{ result }}', $volume, $page);
+} else {
+    $volume = "Hiba! Helytelen adatok vagy üres mezők!";
+
+    $page = str_replace('{{ result }}', $volume, $page);
+}
+
+echo $page;
+echo file_get_contents('templates/foot.html');
 
 ?>
