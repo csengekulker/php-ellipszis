6d81ba0828a77f1c924050666d00edd3fc4169ba
csengekulker <k71608883429@ktch.hu>
2022-05-30 08:53:21 +0200

    Template scripts

---
+++ src/calc.php
@@ -0,0 +1,10 @@
+
+<?php
+
+echo file_get_contents('templates/head.html');
+echo file_get_contents('templates/nav.html');
+echo file_get_contents('templates/calc.html');
+echo file_get_contents('templates/foot.html');
+
+
+?>
